92dc3b2cdfeb69e665a2dd532b0a6cb70b43d426
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2026-02-11 03:23:04 -0700

    Restore direct Lambda handler export

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,29 +1,3 @@
 'use strict';
 
-function fallbackResponse() {
-    return {
-        version: '1.0',
-        response: {
-            outputSpeech: {
-                type: 'SSML',
-                ssml: '<speak>Email Reader is temporarily unavailable. Please try again in a minute.</speak>'
-            },
-            shouldEndSession: true
-        }
-    };
-}
-
-exports.handler = async (event, context, callback) => {
-    try {
-        const skillHandler = require('./src/skill').handler;
-        return skillHandler(event, context, callback);
-    } catch (error) {
-        console.error('Skill bootstrap failure', error);
-        const response = fallbackResponse();
-        if (typeof callback === 'function') {
-            callback(null, response);
-            return;
-        }
-        return response;
-    }
-};
+exports.handler = require('./src/skill').handler;
